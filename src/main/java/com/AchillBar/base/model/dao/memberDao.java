bbc75fbf1b7048a066955303ed1b91e0b76ff144
Dodid54088 <110157359+Dodid54088@users.noreply.github.com>
2022-11-23 22:18:02 +0800

    資展國際專題

---
+++ src/main/java/com/AchillBar/base/model/dao/memberDao.java
@@ -0,0 +1,25 @@
+package com.AchillBar.base.model.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.AchillBar.base.model.memberModel;
+
+public interface memberDao extends JpaRepository<memberModel, Integer> {
+
+	@Query(value = "from memberModel where email = ?1")
+	memberModel findByEmail(String email);
+
+	@Query(value = "from memberModel where phone = ?1")
+	public List<memberModel> findByPhone(String phone);
+
+	@Query(value = "from memberModel where email = ?1 and password = ?2")
+	public memberModel findByEmailAndPassword(String email,String password);
+	
+	public List<memberModel> findAll(); 
+	
+	@Query("From memberModel where admin = 0")
+	public List<memberModel> findAllCustomer();
+}
